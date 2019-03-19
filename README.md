32c73cc54bdea8e9cb083d488215ed4cedcd952c
zhengpeng7 <15732115701@163.com>
2019-03-19 16:36:06 +0800

    Polish up DM generating, train without IN.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SANet_in_Keras
+# SANet-Keras
 > Unofficial implementation of SANet for crowd counting in Keras.
 
 ---
@@ -21,28 +21,46 @@
 
 - **ShanghaiTech dataset**: [dropbox](<https://www.dropbox.com/s/fipgjqxl7uj8hd5/ShanghaiTech.zip?dl=0>) or [Baidu Disk](<http://pan.baidu.com/s/1nuAYslz>).
 
+### Training Parameters:
+
+1. Loss = ssim_loss + L2
+
+2. Optimizer = Adam(lr=1e-4)
+
+3. Data augmentation: Flip horizontally.
+
+4. Patch: No patch, input the whole image, output the same shape DM.
+
+5. Instance normalization: No IN at present.
+
+6. Pay attention to: The density map output may fade to zeros in 90% initialization, I tried the initialization method in the original paper while it didn't work. When this happens, just restart the kernel and re-run... At least, it worked for me, and got the best model in around 70-th epoch. Any other question, contact [me](zhengpeng0108@gmail.com).
+
+7. Loss records:
+
+   ![Loss_records](images/loss_records.png)
+
 ### Run:
 
 1. Download dataset;
-2. Data generation: run the`generate_datasets.ipynb ` to generate formatted data in the `data/`.
+2. Data generation: run the`generate_datasets.ipynb `.
 3. Run the `main.ipynb` to train the model and do the test.
 
-+ #### Abstraction:
+#### Abstraction:
 
-  1. **Network = encoder + decoder**, model plot is [here](./images/SANet_noIN.png):
+1. **Network = encoder + decoder**, model plot is [here](./images/SANet_noIN.png):
 
-     
+   
 
-     |   Network   |           encoder            |               decoder                |
-     | :---------: | :--------------------------: | :----------------------------------: |
-     | Composition |   scale aggregation module   |           conv2dTranspose            |
-     |    Usage    | extract multi-scale features | generate high resolution density map |
+   |   Network   |           encoder            |               decoder                |
+   | :---------: | :--------------------------: | :----------------------------------: |
+   | Composition |   scale aggregation module   |           conv2dTranspose            |
+   |    Usage    | extract multi-scale features | generate high resolution density map |
 
-  2. Loss:
+2. Loss:
 
-     Loss = ![loss_formular](https://latex.codecogs.com/gif.latex?L_{Euclidean}+\alpha_CL_C(L_C=1-\frac{1}{N}{\sum_x{SSIM(x)}},\alpha_C=0.001))
+   Loss = ![loss_formular](https://latex.codecogs.com/gif.latex?L_{Euclidean}+\alpha_CL_C(L_C=1-\frac{1}{N}{\sum_x{SSIM(x)}},\alpha_C=0.001))
 
-  3. Normalization layer:
+3. Normalization layer:
 
-     + Ease the training process;
-     + Reduce 'statistic shift problem'.
+   + Ease the training process;
+   + Reduce 'statistic shift problem'.
--- SANet.py
+++ SANet.py
@@ -1,60 +1,174 @@
-from keras.layers import Conv2D, MaxPooling2D, concatenate, Input, Dense, Conv2DTranspose, ReLU, UpSampling2D
+from keras.layers import Conv2D, MaxPooling2D, concatenate, Input, Dense, Conv2DTranspose, ReLU, UpSampling2D, Activation
 from keras.models import Model
 from keras_contrib.layers import InstanceNormalization
+from keras.initializers import RandomNormal
 
 
-def conv_module(x, num_channel_out, kernel_sizes=(2, 2), padding='same', strides=(1, 1), transpose=False, IN=False):
-    if transpose:
-        x = Conv2DTranspose(num_channel_out, kernel_sizes, strides=strides, padding=padding, use_bias=(not IN))(x)
-        # Substitute conv_transpose with (upsampling + conv)
-        # x = UpSampling2D(size=strides, interpolation='nearest')(x)
-        # x = Conv2D(num_channel_out, kernel_sizes, padding=padding, use_bias=(not IN))(x)
-    else:
-        x = Conv2D(num_channel_out, kernel_sizes, strides=strides, padding=padding, use_bias=(not IN))(x)
-    if IN:
-        x = InstanceNormalization()(x)
-    x = ReLU()(x)
-    return x
+def SANet(IN=True):
+    input_flow = Input((None, None, 3))
+
+    # Module 1
+    channel_out_1 = 16
+    x_1 = Conv2D(channel_out_1, 1, padding='same', use_bias=(not IN))(input_flow)
+    x_1 = Activation('relu')(x_1)
+    x_1 = InstanceNormalization()(x_1) if IN else x_1
+
+    x_2 = Conv2D(channel_out_1*2, 1, padding='same', use_bias=(not IN))(input_flow)
+    x_2 = Activation('relu')(x_2)
+    x_2 = InstanceNormalization()(x_2) if IN else x_2
+    x_2 = Conv2D(channel_out_1, 3, padding='same', use_bias=(not IN))(x_2)
+    x_2 = Activation('relu')(x_2)
+    x_2 = InstanceNormalization()(x_2) if IN else x_2
+
+    x_3 = Conv2D(channel_out_1*2, 1, padding='same', use_bias=(not IN))(input_flow)
+    x_3 = Activation('relu')(x_3)
+    x_3 = InstanceNormalization()(x_3) if IN else x_3
+    x_3 = Conv2D(channel_out_1, 5, padding='same', use_bias=(not IN))(x_3)
+    x_3 = Activation('relu')(x_3)
+    x_3 = InstanceNormalization()(x_3) if IN else x_3
+
+    x_4 = Conv2D(channel_out_1*2, 1, padding='same', use_bias=(not IN))(input_flow)
+    x_4 = Activation('relu')(x_4)
+    x_4 = InstanceNormalization()(x_4) if IN else x_4
+    x_4 = Conv2D(channel_out_1, 7, padding='same', use_bias=(not IN))(x_4)
+    x_4 = Activation('relu')(x_4)
+    x_4 = InstanceNormalization()(x_4) if IN else x_4
+
+    x = concatenate([x_1, x_2, x_3, x_4])
+
+    x = MaxPooling2D()(x)
+
+    # Module 2
+    channel_out_2 = 32
+    x_1 = Conv2D(channel_out_2, 1, padding='same', use_bias=(not IN))(x)
+    x_1 = Activation('relu')(x_1)
+    x_1 = InstanceNormalization()(x_1) if IN else x_1
+
+    x_2 = Conv2D(channel_out_2*2, 1, padding='same', use_bias=(not IN))(x)
+    x_2 = Activation('relu')(x_2)
+    x_2 = InstanceNormalization()(x_2) if IN else x_2
+    x_2 = Conv2D(channel_out_2, 3, padding='same', use_bias=(not IN))(x_2)
+    x_2 = Activation('relu')(x_2)
+    x_2 = InstanceNormalization()(x_2) if IN else x_2
+
+    x_3 = Conv2D(channel_out_2*2, 1, padding='same', use_bias=(not IN))(x)
+    x_3 = Activation('relu')(x_3)
+    x_3 = InstanceNormalization()(x_3) if IN else x_3
+    x_3 = Conv2D(channel_out_2, 5, padding='same', use_bias=(not IN))(x_3)
+    x_3 = Activation('relu')(x_3)
+    x_3 = InstanceNormalization()(x_3) if IN else x_3
+
+    x_4 = Conv2D(channel_out_2*2, 1, padding='same', use_bias=(not IN))(x)
+    x_4 = Activation('relu')(x_4)
+    x_4 = InstanceNormalization()(x_4) if IN else x_4
+    x_4 = Conv2D(channel_out_2, 7, padding='same', use_bias=(not IN))(x_4)
+    x_4 = Activation('relu')(x_4)
+    x_4 = InstanceNormalization()(x_4) if IN else x_4
+
+    x = concatenate([x_1, x_2, x_3, x_4])
+
+    x = MaxPooling2D()(x)
+
+    # Module 3
+    channel_out_3 = 32
+    x_1 = Conv2D(channel_out_3, 1, padding='same', use_bias=(not IN))(x)
+    x_1 = Activation('relu')(x_1)
+    x_1 = InstanceNormalization()(x_1) if IN else x_1
+
+    x_2 = Conv2D(channel_out_3*2, 1, padding='same', use_bias=(not IN))(x)
+    x_2 = Activation('relu')(x_2)
+    x_2 = InstanceNormalization()(x_2) if IN else x_2
+    x_2 = Conv2D(channel_out_3, 3, padding='same', use_bias=(not IN))(x_2)
+    x_2 = Activation('relu')(x_2)
+    x_2 = InstanceNormalization()(x_2) if IN else x_2
+
+    x_3 = Conv2D(channel_out_3*2, 1, padding='same', use_bias=(not IN))(x)
+    x_3 = Activation('relu')(x_3)
+    x_3 = InstanceNormalization()(x_3) if IN else x_3
+    x_3 = Conv2D(channel_out_3, 5, padding='same', use_bias=(not IN))(x_3)
+    x_3 = Activation('relu')(x_3)
+    x_3 = InstanceNormalization()(x_3) if IN else x_3
+
+    x_4 = Conv2D(channel_out_3*2, 1, padding='same', use_bias=(not IN))(x)
+    x_4 = Activation('relu')(x_4)
+    x_4 = InstanceNormalization()(x_4) if IN else x_4
+    x_4 = Conv2D(channel_out_3, 7, padding='same', use_bias=(not IN))(x_4)
+    x_4 = Activation('relu')(x_4)
+    x_4 = InstanceNormalization()(x_4) if IN else x_4
+
+    x = concatenate([x_1, x_2, x_3, x_4])
+
+    x = MaxPooling2D()(x)
+
+    # Module 4
+    channel_out_4 = 32
+    x_1 = Conv2D(channel_out_4, 1, padding='same', use_bias=(not IN))(x)
+    x_1 = Activation('relu')(x_1)
+    x_1 = InstanceNormalization()(x_1) if IN else x_1
+
+    x_2 = Conv2D(channel_out_4*2, 1, padding='same', use_bias=(not IN))(x)
+    x_2 = Activation('relu')(x_2)
+    x_2 = InstanceNormalization()(x_2) if IN else x_2
+    x_2 = Conv2D(channel_out_4, 3, padding='same', use_bias=(not IN))(x_2)
+    x_2 = Activation('relu')(x_2)
+    x_2 = InstanceNormalization()(x_2) if IN else x_2
+
+    x_3 = Conv2D(channel_out_4*2, 1, padding='same', use_bias=(not IN))(x)
+    x_3 = Activation('relu')(x_3)
+    x_3 = InstanceNormalization()(x_3) if IN else x_3
+    x_3 = Conv2D(channel_out_4, 5, padding='same', use_bias=(not IN))(x_3)
+    x_3 = Activation('relu')(x_3)
+    x_3 = InstanceNormalization()(x_3) if IN else x_3
+
+    x_4 = Conv2D(channel_out_4*2, 1, padding='same', use_bias=(not IN))(x)
+    x_4 = Activation('relu')(x_4)
+    x_4 = InstanceNormalization()(x_4) if IN else x_4
+    x_4 = Conv2D(channel_out_4, 7, padding='same', use_bias=(not IN))(x_4)
+    x_4 = Activation('relu')(x_4)
+    x_4 = InstanceNormalization()(x_4) if IN else x_4
+
+    x = concatenate([x_1, x_2, x_3, x_4])
+
+    # Decoder
+
+    x = Conv2D(64, 9, padding='same', use_bias=(not IN))(x)
+    x = Activation('relu')(x)
+    x = InstanceNormalization()(x) if IN else x
+
+    x = Conv2DTranspose(64, kernel_size=(2, 2), strides=(2, 2))(x)
+    x = Activation('relu')(x)
+    x = InstanceNormalization()(x) if IN else x
+
+    x = Conv2D(32, 7, padding='same', use_bias=(not IN))(x)
+    x = Activation('relu')(x)
+    x = InstanceNormalization()(x) if IN else x
+
+    x = Conv2DTranspose(32, kernel_size=(2, 2), strides=(2, 2))(x)
+    x = Activation('relu')(x)
+    x = InstanceNormalization()(x) if IN else x
+
+    x = Conv2D(16, 5, padding='same', use_bias=(not IN))(x)
+    x = Activation('relu')(x)
+    x = InstanceNormalization()(x) if IN else x
+
+    x = Conv2DTranspose(16, kernel_size=(2, 2), strides=(2, 2))(x)
+    x = Activation('relu')(x)
+    x = InstanceNormalization()(x) if IN else x
 
 
-def conv_parallel(x, num_channel_out, reduction_layer=True, IN=False):
-    branches = []
-    kernel_sizes = [1, 3, 5, 7]
-    num_kernel_sizes = len(kernel_sizes)
-    for idx in range(num_kernel_sizes):
-        if reduction_layer and idx > 0:
-            branch = conv_module(x, num_channel_out*2, (1, 1), IN=IN)
-            branch = conv_module(branch, num_channel_out, (kernel_sizes[idx], kernel_sizes[idx]), IN=IN)
-        else:
-            branch = conv_module(x, num_channel_out, (kernel_sizes[idx], kernel_sizes[idx]), IN=IN)
-        branches.append(branch)
-    x = concatenate(branches)
-    return x
+    # Output
 
+    x = Conv2D(16, 3, padding='same', use_bias=(not IN))(x)
+    x = Activation('relu')(x)
+    x = InstanceNormalization()(x) if IN else x
 
-def encoder(x, num_channel_out_lst=[16, 32, 32, 16], IN=False):
-    for idx_num_channel_out in range(len(num_channel_out_lst)):
-        x = conv_parallel(x, num_channel_out_lst[idx_num_channel_out], reduction_layer=(idx_num_channel_out != 0), IN=IN)
-        if idx_num_channel_out < len(num_channel_out_lst) - 1:
-            x = MaxPooling2D()(x)
-    return x
+    # x = Conv2D(16, 5, padding='same', use_bias=(not IN))(x)
+    # x = Activation('relu')(x)
+    # x = InstanceNormalization()(x) if IN else x
 
+    x = Conv2D(1, 1)(x)
+    x = Activation('relu')(x)
 
-def decoder(x, IN=False):
-    kernel_sizes = [9, 7, 5]
-    num_channel_out_lst = [64, 32, 16]
-    for idx in range(len(kernel_sizes)):
-        x = conv_module(x, num_channel_out_lst[idx], (kernel_sizes[idx], kernel_sizes[idx]), IN=IN)
-        x = conv_module(x, num_channel_out_lst[idx], (2, 2), strides=(2, 2), transpose=True, IN=IN)
-    x = conv_module(x, 16, (3, 3), IN=IN)
-    x = conv_module(x, 1, (1, 1))
-    return x
-
-
-def SANet(input_shape=(None, None, 3)):
-    input_flow = Input(input_shape)
-    x = encoder(input_flow, IN=False)
-    x = decoder(x, IN=False)
-    
     model = Model(inputs=input_flow, outputs=x)
+
     return model
